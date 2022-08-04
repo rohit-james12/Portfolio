ff477afad2867d7d3ce64283429790f6010cfcb3
Rohit <rohitjames89@gmail.com>
2022-08-04 23:28:57 +0530

    css

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,12 +4,12 @@ const Contact = () => {
   return (
     <div
       name="contact"
-      className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4"
+      className="w-full h-[800px] md:h-screen bg-[#0a192f] flex justify-center items-center p-4"
     >
       <form
         method="post"
         action="https://getform.io/f/a9c4595e-103a-454f-821d-cc28bd0f924f"
-        className="flex flex-col max-w-[600px] w-full"
+        className="flex flex-col max-w-[600px] w-full text-white"
       >
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-emerald-400 text-emerald-500">
@@ -19,15 +19,20 @@ const Contact = () => {
             Submit the form below or send me an email - rohitjames89@gmail.com
           </p>
         </div>
-        <input className="p-2" name="name" type="text" placeholder="Name" />
         <input
-          className="my-4 p-2"
+          className="p-2 bg-gray-200/20 backdrop-blur outline-none focus:bg-gray-100/30"
+          name="name"
+          type="text"
+          placeholder="Name"
+        />
+        <input
+          className="my-4 p-2 bg-gray-200/20 backdrop-blur outline-none focus:bg-gray-100/30"
           name="email"
           type="email"
           placeholder="Email"
         />
         <textarea
-          className="p-2"
+          className="p-2 bg-gray-200/20 backdrop-blur outline-none focus:bg-gray-100/30"
           name="message"
           rows={10}
           placeholder="Message"
